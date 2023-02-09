3cea5dfb0b3bba01cc1a79620c8555cae437f8d0
AndresGR02 <andres.gr.workspace@gmail.com>
2023-02-09 12:05:37 -0600

    Fix tags
    Fixed some tags, I noticed that tags should be used in a
    hierarchical way. Other changes:
    
    -Added a modal + some Java Script so that each modal can be
    related to the pressed card
    
    -Fixed a problem the cards were having when hovering. Changed
    width for scale

--- main.js
+++ main.js
@@ -1,1 +1,41 @@
 import './style.css'
+
+const buyModal = document.getElementById('buyModal')
+const buyModalBody = document.getElementById('buyModalBody')
+const buyModalTitle = document.getElementById('buyModalTitle')
+
+buyModal.addEventListener('show.bs.modal', e => {
+
+    let price = 0
+    let title = ''
+    switch(e.relatedTarget.id) {
+        case 'donut': 
+            price = 5.99
+            title = 'RETRO DONUTS'
+        break;
+        case 'coco': 
+            price = 3
+            title = 'BEACH TIME'
+        break;
+        case 'carrot': 
+            price = 9
+            title = 'BUNNYS FAVORITE'
+        break;
+        case 'cocoa': 
+            price = 1
+            title = 'HOT COCOA'
+        break;
+        case 'coffee': 
+            price = 2
+            title = 'COSTA RICAN COFFEE'
+        break;
+        case 'churros': 
+            price = 6.99
+            title = 'CHURROS'
+        break;
+    }
+
+    buyModalTitle.innerText = 'BUYING ' + title
+    buyModalBody.innerText = price
+    
+})

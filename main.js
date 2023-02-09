05ded206f79a2ff313b8a740aa6ca67e180b0a85
AndresGR02 <andres.gr.workspace@gmail.com>
2023-02-09 16:49:24 -0600

    Modify buy modal
    Modified the buy modal, you can now choose how much of a certain
    item you want to buy. Other changes:
    
    -Add cart icon
    -Add cart .html

--- main.js
+++ main.js
@@ -3,39 +3,109 @@ import './style.css'
 const buyModal = document.getElementById('buyModal')
 const buyModalBody = document.getElementById('buyModalBody')
 const buyModalTitle = document.getElementById('buyModalTitle')
+const buyPContainer = document.getElementById('buyPContainer')
+const buyRightButton = document.getElementById('buyRightButton')
+const buyLeftButton = document.getElementById('buyLeftButton')
+const buyQuantity = document.getElementById('buyQuantity')
+const addCartBtn = document.getElementById('addCartBtn')
+const addBuyPrice = document.getElementById('buyPrice')
+
+let buyQuantityCount = 0
+let buyTotalPrice = 0
+let lastProductSelected = ''
 
 buyModal.addEventListener('show.bs.modal', e => {
 
-    let price = 0
+    buyQuantityCount = 0
+    buyQuantity.innerText = buyQuantityCount
+    lastProductSelected = e.relatedTarget.id
+
+    buyTotalPrice = 0
+
     let title = ''
-    switch(e.relatedTarget.id) {
+
+    let pInfo = getProductInfo(e.relatedTarget.id)
+    
+    buyModalTitle.innerText = 'BUYING ' + pInfo.title
+    buyPContainer.innerText = 'How many ' + pInfo.title.toLowerCase() + ' would you like to buy?'
+    addBuyPrice.innerText = 'Total $0'
+})
+
+buyLeftButton.addEventListener('click', e => {
+    
+    if(buyQuantityCount != 0) {
+        buyQuantityCount -=1
+        buyQuantity.innerText = buyQuantityCount
+
+        let pInfo = getProductInfo(lastProductSelected)
+
+        buyTotalPrice -= pInfo.price
+        buyTotalPrice = Math.round(buyTotalPrice * 100) / 100
+        addBuyPrice.innerText = 'Total $' + buyTotalPrice 
+    }
+})
+
+buyRightButton.addEventListener('click', e => {
+    if(buyQuantityCount != 100) {
+        buyQuantityCount +=1
+        buyQuantity.innerText = buyQuantityCount
+
+        let pInfo = getProductInfo(lastProductSelected)
+
+        buyTotalPrice += pInfo.price
+        buyTotalPrice = Math.round(buyTotalPrice * 100) / 100
+        addBuyPrice.innerText = 'Total $' + buyTotalPrice 
+    }
+        
+})
+
+addCartBtn.addEventListener('click', e => {
+    console.log('g')
+})
+
+function getProductInfo(pName) {
+
+    let pInfo 
+
+    switch(pName) {
         case 'donut': 
-            price = 5.99
-            title = 'RETRO DONUTS'
+            pInfo = {
+                price: 5.99,
+                title: 'RETRO DONUTS',
+            }
         break;
         case 'coco': 
-            price = 3
-            title = 'BEACH TIME'
+            pInfo = {
+                price: 3,
+                title: 'BEACH TIME',
+            }
         break;
         case 'carrot': 
-            price = 9
-            title = 'BUNNYS FAVORITE'
+            pInfo = {
+                price: 9,
+                title: 'BUNNYS FAVORITE',
+            }
         break;
-        case 'cocoa': 
-            price = 1
-            title = 'HOT COCOA'
+        case 'cocoa':
+            pInfo = {
+                price: 1,
+                title: 'HOT COCOA',
+            }
+
         break;
         case 'coffee': 
-            price = 2
-            title = 'COSTA RICAN COFFEE'
+            pInfo = {
+                price: 2,
+                title: 'COSTA RICAN COFFEE',
+            }
         break;
         case 'churros': 
-            price = 6.99
-            title = 'CHURROS'
+            pInfo = {
+                price: 6.99,
+                title: 'CHURROS',
+            }
         break;
     }
 
-    buyModalTitle.innerText = 'BUYING ' + title
-    buyModalBody.innerText = price
-    
-})
+    return pInfo
+}
